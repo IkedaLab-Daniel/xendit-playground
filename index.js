6e61b68880ae7698f9c4f1c45f4f3855e63d3393
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-02-04 23:58:08 +0800

    feat: implement create-invoice endpoint with basic auth and axios integration

--- index.js
+++ index.js
@@ -8,6 +8,39 @@ app.use(express.json());
 const XENDIT_KEY = process.env.XENDIT_SECRET_KEY;
 const PORT = 8070;
 
+// > Basic auth: username = secret key, password = blank
+const authHeader = "Basic " + Buffer.from(`${XENDIT_KEY}`).toString("base64");
+
+// > Create an invoice
+app.post("/create-invoice", async (req, res) => {
+    try {
+         // ? sample
+         const { amount = 100, email = "sample@gmail.com"} = req.body;
+
+         const external_id = `order-${Date.now()}`;
+
+         const resp = await axios.post(
+            "https://api.xendit.co/v2/invoices",
+            {
+                external_id,
+                amount, // > 100 -> PHP 100
+                description: "sana gumana",
+                success_redirect_url: `http://localhost:${PORT}/success`,
+                failure_redirect_url: `http://localhost:${PORT}/failure`
+            },
+            {
+                headers: {
+                    Authorization: authHeader,
+                    "Content-Type": "application/json",
+                },
+            }
+         );
+         
+    } catch (ice) {
+
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Running on http://localhost:${PORT}`);
 })
